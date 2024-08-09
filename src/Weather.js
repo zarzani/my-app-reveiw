91841915a7fb9d2429b1f12fbc9e0d83943fd877
zarzani <zahraarzani@gmail.com>
2024-08-09 17:42:55 +0330

    color of cities

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,10 @@
 import React from "react";
+import "./Weather.css"
 export default function Weather (props) {
     return (
         <div className="Temperature">
-            <h2> The Temperture in {props.City} is {props.value} C </h2>
+            <h2> The Temperture in <span className="City"> {props.City}</span>  is {props.value} °C
+            </h2>
         </div>
     )
 }
