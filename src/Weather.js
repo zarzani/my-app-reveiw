273cec014258e4e31eb96dfa4792327e35519df0
zarzani <zahraarzani@gmail.com>
2024-08-12 21:41:47 +0330

    far to celsius

--- src/Weather.js
+++ src/Weather.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Weather.css";
 import Humidity from "./Humidity";
 export default function Weather (props) {
+    let [temperature, setTemperature] = useState (props.temperature);
+    let [unit, setUnit] = useState ("Metric");
+      
+
     function showFahrenhait (event){
         event.preventDefault();
-        let temperature = (props.temperature * 9) / 5 + 32;
-    alert (`The temperature in fahrenhait is ${temperature} °F`);
-    }
+        setUnit ("Imperial-fahrenhait");
+        setTemperature ((props.temperature * 9) / 5 + 32);
+     }
+     function showCelsius (event) {
+        event.preventDefault();
+        setUnit("Metric-celsius")
+        setTemperature (props.temperature);
+     }
     if (props.temperature >= 20) {
          return (
         <div className="Temperature">
-            <h2> The Temperture in <span className="City"> {props.City}</span> is {props.temperature} °C  (warm) /<a href="/" onClick={showFahrenhait} > °F </a>
+            <h2> The Temperture in <span className="City"> {props.City}</span> is {temperature} {unit} <a href="/" onClick = {showCelsius} > °C </a>  (warm) <a href="/" onClick={showFahrenhait} > °F </a>
              < Humidity value ={props.humidity}/>%
             </h2>
         </div>
-    );}
+    );
+}
     else {
         return (
-            <h2> The Temperture in <span className="City"> {props.City}</span> is {props.temperature} °C  (not so warm) /<a href="/" onClick={showFahrenhait} > °F </a>
+            <h2> The Temperture in <span className="City"> {props.City}</span> is {temperature} <a href="/" onClick = {showCelsius} > °C </a>  (not so warm) <a href="/" onClick={showFahrenhait} > °F </a>
              < Humidity value ={props.humidity}/>%
             </h2>
         );
