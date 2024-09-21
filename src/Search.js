33adbc0095f2fa16328730c99fdbe4e1e1558c92
zarzani <zahraarzani@gmail.com>
2024-09-21 15:31:43 +0330

    form complited

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Search.css";
 
 export default function Search (){
+    let [city, setCity] = useState (" ");
+    function handleSubmite (event) {
+        event.preventDefault ( );
+        alert (`Search for ${city}`);
+    }
+    function updateCity(event) {
+        setCity(event.target.value);
+    }
     return (
     <p> 
-        <form>
-            <input type="Search" />
-            <input type="Submit" value = "search" />
+        <form onSubmit={handleSubmite}>
+            <input type="Search" onChange={updateCity} />
+            <input type="Submit"   value = "search" />
             
         </form>
     </p>
